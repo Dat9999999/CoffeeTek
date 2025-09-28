1da1d3243255be647652b6c34a6bb97a7cddf8d1
zdat5624 <zdat5624@email.com>
2025-09-28 13:59:34 +0700

    update fe

--- Frontend/lib/constant/api.constant.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
-export const API_ENDPOINTS = {
-    AUTH: {
-        LOGIN: `${BASE_URL}/auth/login`,
-        SIGNUP: `${BASE_URL}/auth/signup`,
-    },
-};
--- Frontend/utils/auth.js
+++
@@ -1,5 +0,0 @@
-import { STORAGE_KEYS } from "@/lib/constant/storageKey.constant";
-export function getAccessToken() {
-    if (typeof window === "undefined") return null;
-    return localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN);
-}

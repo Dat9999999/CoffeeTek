72fa110974f60a86e7d9a7447d1bf0b954f035d4
Dat9999999 <huynhtandat184@gmail.com>
2025-10-12 13:57:16 +0700

    rename env of frontend

--- fe-coffetek-new/src/lib/api.ts
+++ fe-coffetek-new/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api", // backend url
+    baseURL: process.env.NEXT_PUBLIC_ENDPOINT_API || "http://localhost:3001/api", // backend url
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",

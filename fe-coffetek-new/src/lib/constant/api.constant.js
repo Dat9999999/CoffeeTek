e68d27fdf207d4586cb1c5f5bb7bf96f94ca80d9
zdat5624 <zdat5624@email.com>
2025-10-14 00:34:12 +0700

    fix bug to build

--- fe-coffetek-new/src/lib/constant/api.constant.js
+++ fe-coffetek-new/src/lib/constant/api.constant.js
@@ -1,4 +1,4 @@
-// const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
+// const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001/api";
 // export const API_ENDPOINTS = {
 //     AUTH: {
 //         LOGIN: `${BASE_URL}/auth/login`,
@@ -8,13 +8,13 @@
 
 export const API_ENDPOINTS = {
   AUTH: {
-    LOGIN: `${process.env.NEXT_PUBLIC_ENDPOINT_API}/auth/login`,
-    SIGNUP: `${process.env.NEXT_PUBLIC_ENDPOINT_API}/auth/signup`,
-    CHANGE_PASSWORD: `${process.env.NEXT_PUBLIC_ENDPOINT_API}/auth/change-password`,
+    LOGIN: `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/login`,
+    SIGNUP: `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/signup`,
+    CHANGE_PASSWORD: `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/change-password`,
   },
 
   USER: {
-    PROFILE: `${process.env.NEXT_PUBLIC_ENDPOINT_API}/user/me`,
+    PROFILE: `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/me`,
   },
 };
 

169ab90a326f7892c92830fc63fdbd7866dae61d
Dat9999999 <huynhtandat184@gmail.com>
2025-10-23 14:40:02 +0700

    BE: createLoyalLevelDTO

--- Backend/src/loyal-level/dto/create-loyal-level.dto.ts
+++ Backend/src/loyal-level/dto/create-loyal-level.dto.ts
@@ -1,1 +1,12 @@
-export class CreateLoyalLevelDto {}
+import { IsNotEmpty, IsNumber, Min } from "class-validator"
+
+export class CreateLoyalLevelDto {
+    @IsNotEmpty()
+    name: string
+
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(1)
+    requirePoint: number
+}
--- Backend/src/loyal-level/loyal-level.service.ts
+++ Backend/src/loyal-level/loyal-level.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLoyalLevelDto } from './dto/create-loyal-level.dto';
 import { UpdateLoyalLevelDto } from './dto/update-loyal-level.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class LoyalLevelService {
+  constructor(private readonly prisma: PrismaService) { }
   create(createLoyalLevelDto: CreateLoyalLevelDto) {
     return 'This action adds a new loyalLevel';
   }
 
-  findAll() {
-    return `This action returns all loyalLevel`;
+  async findAll() {
+    return await this.prisma.loyalLevel.findMany();
   }
 
   findOne(id: number) {

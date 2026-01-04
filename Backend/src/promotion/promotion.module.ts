4a26487dc4ece678b8ece37ad5cf6ac534cfd396
Dat9999999 <huynhtandat184@gmail.com>
2026-01-04 11:04:15 +0700

    setup sender MQ

--- Backend/src/promotion/promotion.module.ts
+++ Backend/src/promotion/promotion.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { PromotionService } from './promotion.service';
 import { PromotionController } from './promotion.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,
+    ClientsModule.register([
+      {
+        name: 'PROMOTION_MAIL_SERVICE', // Define a name for the microservice
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://127.0.0.1:5672'], // IP and port of RabbitMQ server
+          queue: 'promotion_emails_queue',
+          queueOptions: {
+            durable: true, // Queue will not lose data when RabbitMQ restarts
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [PromotionController],
   providers: [PromotionService],
 })
